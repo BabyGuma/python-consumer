54e127ab6083f9349146d77434597991801cda9e
BabyGuma <zS20020316@estudiantes.uv.mx>
2023-05-29 19:14:06 -0600

    update

--- kafka-consumer-mongo.py
+++ kafka-consumer-mongo.py
@@ -5,16 +5,14 @@
 from kafka import KafkaConsumer
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
+
 import json
-import subprocess
 
-
-
-# replace here with your mongodb url 
 uri = "mongodb+srv://Guma:1234@cloud.g2vb4oz.mongodb.net/?retryWrites=true&w=majority"
+#Local = "mongodb://127.0.0.1:27017"
+#URL del profe = "mongodb+srv://adsoft:adsoft-sito@cluster0.kzghgph.mongodb.net/?retryWrites=true&w=majority"
 
-
-# Create a new client and connect to the serve
+# Create a new client and connect to the server
 #client = MongoClient(uri, server_api=ServerApi('1'))
 # Send a ping to confirm a successful connection
 
@@ -27,7 +25,7 @@ uri = "mongodb+srv://Guma:1234@cloud.g2vb4oz.mongodb.net/?retryWrites=true&w=maj
 # Connect to MongoDB and pizza_data database
 
 try:
-    client = MongoClient(uri, server_api=ServerApi('1'))
+    client = MongoClient(uri)
     client.admin.command('ping')
     print("Pinged your deployment. You successfully connected to MongoDB!")
 
@@ -35,10 +33,7 @@ try:
     print("MongoDB Connected successfully!")
 except:
     print("Could not connect to MongoDB")
-
-consumer = KafkaConsumer('test',bootstrap_servers=[
-     'my-kafka-0.my-kafka-headless.babyguma.svc.cluster.local:9092'
-    ])
+consumer = KafkaConsumer('test',bootstrap_servers=['my-kafka-0.my-kafka-headless.babyguma.svc.cluster.local:9092'])
 # Parse received data from Kafka
 for msg in consumer:
     record = json.loads(msg.value)
@@ -51,8 +46,28 @@ for msg in consumer:
        print (meme_rec)
        meme_id = db.memes_info.insert_one(meme_rec)
        print("Data inserted with record ids", meme_id)
-
-       subprocess.call(['sh', './test.sh'])
-
     except:
        print("Could not insert into MongoDB")
+
+    try:
+        agg_result = db.memes_info.aggregate(
+            [{
+                "$group" : 
+                { "_id" : "$name",
+                  "n" : {"$sum":1}}
+            }]
+        )
+        db.memes_summary.delete_many({})
+        for i in agg_result:
+            print(i)
+            summary_id = db.memes_summary.insert_one(i)
+            print("Summary inserted with record ids", summary_id)
+       #meme_rec = {'name':name }
+       #print (meme_rec)
+       #meme_id = db.memes_info.insert_one(meme_rec)
+       #print("Data inserted with record ids", meme_id)
+    except Exception as e:
+        print(f'group by caught {type(e)}: ')
+        print(e)
+       #print("Could not insert into MongoDB")
+
