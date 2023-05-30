ab30f3cdd7cea1d2b96534c4129d22d7a6574fdc
BabyGuma <zS20020316@estudiantes.uv.mx>
2023-05-29 19:27:15 -0600

    update2

--- kafka-consumer-mongo.py
+++ kafka-consumer-mongo.py
@@ -33,7 +33,7 @@ try:
     print("MongoDB Connected successfully!")
 except:
     print("Could not connect to MongoDB")
-consumer = KafkaConsumer('test',bootstrap_servers=['my-kafka-0.my-kafka-headless.babyguma.svc.cluster.local:9092'])
+consumer = KafkaConsumer('test',bootstrap_servers=['my-kafka.babyguma.svc.cluster.local:9092'])
 # Parse received data from Kafka
 for msg in consumer:
     record = json.loads(msg.value)
